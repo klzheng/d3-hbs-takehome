33d89d6901c8cb3b4cc6eca656635301d045004c
Kevin <klzheng@buffalo.edu>
2023-02-02 18:48:22 -0500

    Created header component Added styling to background, container, and header card Added various image assets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'time-tracking-dashboard';
+  title = 'Time Tracking Dashboard';
 }
---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+
+export class HeaderComponent {
+  @Input() name: string = "";
+  @Input() imgPath: string = "";
+
+
+  title = "Time Tracking Dashboard"
+}
